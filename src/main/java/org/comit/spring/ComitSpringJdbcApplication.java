d686a576392f305e1c7f930c1a19e99bb34bb0d6
sam obadan <obadansam@gmail.com>
2024-04-14 23:05:16 +0100

    first commit

---
+++ src/main/java/org/comit/spring/ComitSpringJdbcApplication.java
@@ -0,0 +1,13 @@
+package org.comit.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComitSpringJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComitSpringJdbcApplication.class, args);
+	}
+
+}
